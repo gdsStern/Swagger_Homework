0aa03e1dc1fd7266a0a0394aadffdd720fd1a858
User <git@github.com:gdsStern/HelloWorld.git>
2024-07-28 22:19:29 +0300

    Сделано ДЗ

--- src/main/java/ru/hogwarts/school/controller/StudentController.java
+++ src/main/java/ru/hogwarts/school/controller/StudentController.java
@@ -1,20 +1,28 @@
 package ru.hogwarts.school.controller;
 
 import io.swagger.v3.oas.annotations.parameters.RequestBody;
+import org.springframework.data.util.Pair;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import ru.hogwarts.school.model.Faculty;
 import ru.hogwarts.school.model.Student;
+import ru.hogwarts.school.service.AvatarService;
 import ru.hogwarts.school.service.StudentService;
 
+import java.io.IOException;
 import java.util.List;
 
 @RequestMapping("/student")
 @RestController
 public class StudentController {
     private final StudentService studentService;
+    private final AvatarService avatarService;
 
-    public StudentController(StudentService studentService) {
+    public StudentController(StudentService studentService, AvatarService avatarService) {
         this.studentService = studentService;
+        this.avatarService = avatarService;
     }
 
     @PostMapping
@@ -50,5 +58,22 @@ public class StudentController {
     @GetMapping("/{id}/faculty")
     public Faculty findStudentsFaculty(@PathVariable Long id) {
         return studentService.findStudentsFaculty(id);
+    }
+    @GetMapping("/{id}/avatarFromDb")
+    public ResponseEntity<byte[]> getAvatarFromDb(@PathVariable Long id) {
+        return buildResponseEntity(avatarService.getAvatarFromDb(id));
+    }
+    @GetMapping("/{id}/avatarFromFs")
+    public ResponseEntity<byte[]> getAvatarFromFs(@PathVariable Long id) throws IOException {
+        return buildResponseEntity(avatarService.getAvatarFromFs(id));
+    }
+
+    public ResponseEntity<byte[]> buildResponseEntity(Pair<byte[], String> pair) {
+        byte[] data = pair.getFirst();
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .contentLength(data.length)
+                .contentType(MediaType.parseMediaType(pair.getSecond()))
+                .body(data);
     }
 }
