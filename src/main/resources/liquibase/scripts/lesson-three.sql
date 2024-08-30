1280da44238d98c4ff7b5da9feba54a1037be35e
User <git@github.com:gdsStern/HelloWorld.git>
2024-08-30 17:13:14 +0300

    Исправлено ДЗ

--- src/main/resources/liquibase/scripts/lesson-three.sql
+++ src/main/resources/liquibase/scripts/lesson-three.sql
@@ -1,2 +1,6 @@
+-- liquibase formatted sql
+
+-- changeset jrembo:1
 create index students_name_index on students (name);
+-- changeset jrembo:2
 create index faculties_nameColor_index on faculties (name, color);
